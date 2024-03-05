f1b307b64eeecaaec9780ed2fb301ee15805d1ad
Thomas Kibby <htkibby@gmail.com>
2024-03-04 18:13:25 -0800

    Add project files.

---
+++ AdventOfCode2015Part3/Coordinate.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2015Part3
+{
+    public class Coordinate
+    {
+        public int x = 0; 
+        public int y = 0;
+    }
+}
---
+++ AdventOfCode2015Part3/Program.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode2015Part3;
+
+string filePath =@"AdventOfCode2015Part3\\Directions.txt";
+StreamReader directions = new(filePath);
+List<Coordinate> coordinates = [];
+
+while (!directions.EndOfStream)
+{
+    int x = 0;
+    int y = 0;
+    string cardinalDirections = directions.ReadLine();
+
+    foreach (char direction in cardinalDirections)
+    {
+        if (direction == '^')
+        {
+            coordinates.Add(new Coordinate { x = x, y = y++ });
+        }
+        else if (direction == 'v')
+        {
+            coordinates.Add(new Coordinate { x = x, y = y-- });
+        }
+        else if (direction == '>')
+        {
+            coordinates.Add(new Coordinate { x = x++, y = y });
+        }
+        else if (direction == '<') 
+        {
+            coordinates.Add(new Coordinate { x = x--, y = y });
+        }
+    }
+}
+
+List<Coordinate> distinctCoordinates = coordinates.DistinctBy(c => new {c.x, c.y}).ToList();
+
+Console.WriteLine($"{distinctCoordinates.Count}");
